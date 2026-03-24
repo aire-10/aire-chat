bd06d519e6115b6ef484ba9fc6ebdfaf7c4af704
Qilah <24ftt1082@student.pb.edu.bn>
2026-03-24 22:14:42 +0800

    full project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aire-chat/project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e4cb41da2fe044fb9802990459ec42a8cd923c5d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aire-chat/project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e4cb41da2fe044fb9802990459ec42a8cd923c5d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
